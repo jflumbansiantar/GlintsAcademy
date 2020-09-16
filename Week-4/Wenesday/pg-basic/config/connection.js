e21d4823e09cec95d91b0aca976f0751fbfd80c6
Julia Fransiska L <jflumbansiantar@gmail.com>
2020-09-16 15:23:10 +0700

    Create database, SUCCES

--- Week-4/Wenesday/pg-basic/config/connection.js
+++ Week-4/Wenesday/pg-basic/config/connection.js
@@ -1,1 +1,12 @@
-const {Pool} = require
+const {Pool} = require('pg');
+const pool = new Pool({
+    database: 'pg-basic',
+    host: 'localhost',
+    user: 'postgres',
+    password: 'Idjoel3007',
+    port: 5432,
+}
+
+);
+
+module.exports = pool;
--- Week-4/Wenesday/pg-basic/setup.js
+++ Week-4/Wenesday/pg-basic/setup.js
@@ -1,8 +1,20 @@
 const pool = require('./config/connection');
 
-const productsTableSql = '
-CREATE TABLE teachers (
+const productsTableSql = `
+CREATE TABLE products (
     id SERIAL PRIMARY KEY,
-    
-    )
-';
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    gender VARCHAR(6)
+);
+`;
+
+pool.query(productsTableSql, (err, data) => {
+    if(err){
+        throw err;
+    } else {
+        console.log('Table "products" created successfuly')
+        pool.end();
+    }
+});
