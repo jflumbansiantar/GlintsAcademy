7f379f6391649e00393837e2f7783c95611b01ae
Julia Fransiska L <jflumbansiantar@gmail.com>
2020-10-29 12:30:22 +0700

    findthesheep

--- Hackerank/sum_array.js
+++ Hackerank/sum_array.js
@@ -1,11 +1,12 @@
 let ar = [1, 2, 3, 4, 10, 11]
+let ar1 = ["1 2 3 4 10 11"]
 
-function simpleArithmetic(ar){
-    let arSum = 0;
-    for (let i = 0; i < ar.length; i++){
-        arSum += ar[i]
+function simpleArithmetic(){
+    let arSum = 0
+    for (let i = 0; i < ar1.length; i++){
+        arSum += ar1[i]
     }
     return arSum;
 }
 
-console.log(simpleArithmetic(ar))
+console.log(simpleArithmetic(ar1))
