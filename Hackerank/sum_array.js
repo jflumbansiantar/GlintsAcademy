bb11421872b435095fcc979c8a21b949d5eca139
Julia Fransiska L <jflumbansiantar@gmail.com>
2020-11-09 10:45:43 +0700

    fixing

--- Hackerank/sum_array.js
+++ Hackerank/sum_array.js
@@ -1,9 +1,9 @@
 let ar = [1, 2, 3, 4, 10, 11]
 let ar1 = ["1 2 3 4 10 11"]
 
-function simpleArithmetic(){
+function simpleArithmetic() {
     let arSum = 0
-    for (let i = 0; i < ar1.length; i++){
+    for (let i = 0; i < ar1.length; i++) {
         arSum += ar1[i]
     }
     return arSum;
