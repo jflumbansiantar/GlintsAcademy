a1e53f2c5d90303205ac720671995a1a4c926393
Julia Fransiska L <jflumbansiantar@gmail.com>
2020-11-11 15:23:35 +0700

    lalalala

--- Hackerank/find_the_sheep.js
+++ Hackerank/find_the_sheep.js
@@ -34,54 +34,54 @@ console.log(warnTheSheep(where2));
 console.log(warnTheSheep(where3));
 console.log(warnTheSheep(where4));
 
-function countTheSheep(data) {
-    let sheeps = 0;
-    for (let i = 0; i <= data.length; i++) {
-        let animal = data[i];
-        if (animal === "sheep") {
-            sheeps++;
-        }
-    }
-    return `We have ${sheeps} sheeps.`;
-}
-console.log(countTheSheep(where));
-console.log(countTheSheep(where1));
-console.log(countTheSheep(where2));
-console.log(countTheSheep(where3));
-console.log(countTheSheep(where4));
+// function countTheSheep(data) {
+//     let sheeps = 0;
+//     for (let i = 0; i <= data.length; i++) {
+//         let animal = data[i];
+//         if (animal === "sheep") {
+//             sheeps++;
+//         }
+//     }
+//     return `We have ${sheeps} sheeps.`;
+// }
+// console.log(countTheSheep(where));
+// console.log(countTheSheep(where1));
+// console.log(countTheSheep(where2));
+// console.log(countTheSheep(where3));
+// console.log(countTheSheep(where4));
 
-function countTheWolf(data) {
-    let wolfs = 0;
-    for (let i = 0; i <= data.length; i++) {
-        let animal = data[i];
-        if (animal === "wolf") {
-            wolfs++;
-        }
-    }
-    if (wolfs > 1) {
-        return `Why we have ${wolfs} wolfs?!`;
-    } else {
-        return `Why we have ${wolfs} wolf?!`
-    }
+// function countTheWolf(data) {
+//     let wolfs = 0;
+//     for (let i = 0; i <= data.length; i++) {
+//         let animal = data[i];
+//         if (animal === "wolf") {
+//             wolfs++;
+//         }
+//     }
+//     if (wolfs > 1) {
+//         return `Why we have ${wolfs} wolfs?!`;
+//     } else {
+//         return `Why we have ${wolfs} wolf?!`
+//     }
 
-}
-console.log(countTheWolf(where));
-console.log(countTheWolf(where1));
-console.log(countTheWolf(where2));
-console.log(countTheWolf(where3));
-console.log(countTheWolf(where4));
+// }
+// console.log(countTheWolf(where));
+// console.log(countTheWolf(where1));
+// console.log(countTheWolf(where2));
+// console.log(countTheWolf(where3));
+// console.log(countTheWolf(where4));
 
-function countThemAll(data) {
-    let all = {};
-    for (let i = 0; i < data.length; i++) {
-        let animal = data[i];
-        all[animal] = (all[animal] + 1) || 1;
-    }
-    return all;
-}
+// function countThemAll(data) {
+//     let all = {};
+//     for (let i = 0; i < data.length; i++) {
+//         let animal = data[i];
+//         all[animal] = (all[animal] + 1) || 1;
+//     }
+//     return all;
+// }
 
-console.log(countThemAll(where));
-console.log(countThemAll(where1));
-console.log(countThemAll(where2));
-console.log(countThemAll(where3));
-console.log(countThemAll(where4));
+// console.log(countThemAll(where));
+// console.log(countThemAll(where1));
+// console.log(countThemAll(where2));
+// console.log(countThemAll(where3));
+// console.log(countThemAll(where4));
