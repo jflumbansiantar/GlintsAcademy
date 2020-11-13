ef595012c28d95f0cc86f7e0ff407c37e1efbc7d
Julia Fransiska L <jflumbansiantar@gmail.com>
2020-11-13 20:06:11 +0700

    added

--- Tugas/Latihan_LiveCode/soal2.js
+++ Tugas/Latihan_LiveCode/soal2.js
@@ -4,10 +4,22 @@
 const retjehan = [100000, 50000, 20000, 5000, 1000, 500, 100]
 
 const exchange = (doit) => {
-    let temp = {};
-    // while (condition) {
-        
+    let temp = [];
+    // temp.a = 
+    for (let i = 0; i <= retjehan.length; i++) {
+        for (let j = 0; j <= retjehan.length; j++) {
+            doit = doit % retjehan[i];
+            if (retjehan[i] = retjehan[j]) {
+                temp.push(retjehan[i])
+            }
+        }
+
     }
+    console.log(doit, '--doit')
+
+    console.log(temp, '--temporary Data')
+    // return temp;
+}
 
 //Test Case
 exchange(185500);
@@ -21,7 +33,7 @@ exchange(185500);
         500 : 1
     }
 */
-exchange(377000);
+// exchange(377000);
 // Output
 /*
      {
--- Tugas/Latihan_LiveCode/soal6.js
+++ Tugas/Latihan_LiveCode/soal6.js
@@ -30,31 +30,6 @@ const solvedWord = (sentence) => {
     }
 }
 
-// const code = ["alpha", "bravo", "charlie", "delta", "echo"] //Lanjutkan sendiri
-
-// const solvedWord = (codes) => {
-//     //code here
-// }
-
-// const codes = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliet", "kilo", "lima", "mike", "nano", "oscar", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "xray", "yankee", "zulu "] //Lanjutkan sendiri
-
-// const solvedWord = (sentence) => {
-//     //code here
-//     let words = sentence.split(" ");
-//     let output = "";
-//     words.forEach(word => {
-//         codes.forEach(code => {
-//             if (word == code) {
-//                 output += word[0].toUpperCase()
-//             }
-//         })
-//     })
-//     if (!output) {
-//         console.log(`Code can't be solved`);
-//     } else {
-//         console.log(output);
-//     }
-// }
 
 
 //Test Case
